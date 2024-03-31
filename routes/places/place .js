44cab35305ff1cd4d196d6d85764d9c489c5f64a
eslam <ealaa3764@gmail.com>
2024-03-31 21:38:09 +0200

    fixatef

--- routes/places/place .js
+++ routes/places/place .js
@@ -20,6 +20,22 @@ const express = require("express"),
 });
 
 
+router.get("/places/:city/:limit/:pageN", async (req, res) => {
+  try {
+      let size = req.params.limit;
+      let pageN = req.params.pageN;
+      let city = req.params.city;
+
+      let result = await client.query("SELECT p.name, p.address, p.description, p.hour_salary, pi.image FROM places p LEFT JOIN places_images pi ON p.id = pi.place_id WHERE p.active = true AND p.city = $1 AND  LIMIT $2 OFFSET $3", [city, size, (pageN - 1) * size]);
+
+      res.json(result.rows);
+  } catch (error) {
+      console.error(error); // Log the error for debugging
+      res.status(500).json({ error: 'Internal server error' }); // Send an error response
+  }
+});
+
+
 
 
 
